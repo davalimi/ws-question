5bdc8b95835f84dc007d8091690d43f88bb32847
david alimi <davealimi@gmail.com>
2021-02-07 14:26:31 +0200

    auto-install node

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+## INSTALL NODE if needed
+if which node > /dev/null
+then
+    echo '';
+else
+    echo -e "Installing node.IO ...";
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+    sudo apt -y install nodejs npm;
+fi
+
 ###RUN ON PORT 3000
 node index.js -r . -p 3000 --oc 1
 
